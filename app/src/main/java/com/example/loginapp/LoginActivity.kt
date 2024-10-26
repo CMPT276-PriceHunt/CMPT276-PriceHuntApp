6364db79014d4e85fac2dec6fd3964d6cf45d053
Willy-Ho <williamho8813@gmail.com>
2024-10-25 22:45:13 -0700

    slowly adding everything

--- app/src/main/java/com/example/loginapp/LoginActivity.kt
+++ app/src/main/java/com/example/loginapp/LoginActivity.kt
@@ -10,16 +10,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var usernameInput :EditText
     private lateinit var passwordInput : EditText
     private lateinit var loginBtn :Button
+    private lateinit var signUpLink : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         usernameInput = findViewById(R.id.username_input)
         passwordInput = findViewById(R.id.password_input)
         loginBtn = findViewById(R.id.login_btn)
+        signUpLink = findViewById(R.id.signUpLink)
         loginBtn.setOnClickListener{
             val username = usernameInput.text.toString()
             val password = passwordInput.text.toString()
