3e6824dea82df0fc2294811d3a69e390bfb54302
hzzhub <2625758647@qq.com>
2024-10-30 17:34:38 -0700

    add comments
    1

--- app/src/main/java/com/example/loginapp/WishlistItem.java
+++ app/src/main/java/com/example/loginapp/WishlistItem.java
@@ -4,22 +4,26 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class WishlistItem implements Parcelable {
-    private String imagePath;
-    private String name;
-    private double price;
+    // Basic properties of a wishlist item
+    private String imagePath;  // Path to item's image
+    private String name;       // Name of the item
+    private double price;      // Price of the item
 
+    // Standard constructor for creating new items
     public WishlistItem(String imagePath, String name, double price) {
         this.imagePath = imagePath;
         this.name = name;
         this.price = price;
     }
 
+    // Parcel constructor for recreating object
     protected WishlistItem(Parcel in) {
         imagePath = in.readString();
         name = in.readString();
         price = in.readDouble();
     }
 
+    // Parcelable implementation
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(imagePath);
@@ -32,6 +36,7 @@ public class WishlistItem implements Parcelable {
         return 0;
     }
 
+    // Parcelable CREATOR for object creation from Parcel
     public static final Creator<WishlistItem> CREATOR = new Creator<WishlistItem>() {
         @Override
         public WishlistItem createFromParcel(Parcel in) {
@@ -44,6 +49,7 @@ public class WishlistItem implements Parcelable {
         }
     };
 
+    // Getters for item properties
     public String getImagePath() { return imagePath; }
     public String getName() { return name; }
     public double getPrice() { return price; }
